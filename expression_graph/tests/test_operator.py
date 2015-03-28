a113fb0e5d352784f2060a68f7e1ce70675a4bd3
Jay Osako <JosephOsako@gmail.com>
2015-03-28 10:19:04 -0400

    Added more tests for Operator classes.

--- expression_graph/tests/test_operator.py
+++ expression_graph/tests/test_operator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from fractions import Fraction
-from expression_graph.operator import IntegralOperator, RationalOperator, \
-    RealOperator
+from expression_graph.operator import Operator, IntegralOperator, \
+    RationalOperator, RealOperator, UnsupportedOperatorException
 
 import unittest
 
@@ -10,20 +10,36 @@ class IntegralOperatorTest(unittest.TestCase):
         addop = IntegralOperator('+')
         self.assertEqual(4, addop.apply(2, 2))
 
+    def testUnsupportedOperator(self):
+        self.assertRaises(UnsupportedOperatorException,
+                          IntegralOperator, '**')
+
+    def testDivisionPromoteToFraction(self):
+        divop = IntegralOperator('/')
+        quotient = divop.apply(4, 6)
+        self.assertEqual(type(quotient), Fraction)
+        self.assertEqual(Fraction(2, 3), quotient)
 
         
 class RationalOperatorTest(unittest.TestCase):
     def testSimpleApplication(self):
         addop = RationalOperator('+')
         self.assertEqual(1, addop.apply(Fraction(1, 2), Fraction(1, 2)))
+        
+    def testUnsupportedOperator(self):
+        self.assertRaises(UnsupportedOperatorException,
+                          RationalOperator, '**')
 
-
+        
         
 class RealOperatorTest(unittest.TestCase):
     def testSimpleApplication(self):
         addop = RealOperator('+')
         self.assertEqual(4.0, addop.apply(2.0, 2.0))
-
+        
+    def testUnsupportedOperator(self):
+        self.assertRaises(UnsupportedOperatorException,
+                          RealOperator, '**')
 
         
 if __name__ == '__main__':
