3b4dc64fc8d121656f179abaf9db783a221c71a6
renacuajo67 <rene@ieee.org>
2015-04-03 18:35:13 -0400

    Check point my work

--- tree.py
+++ tree.py
@@ -39,22 +39,22 @@ def reduce(tokenizer):
                 return accum
 
     elif toktyp == tokenize.NUMBER:
-        print "Habemus NUMBER " + tokval
+        # print "Habemus NUMBER " + tokval
         return int(tokval)
     elif toktyp == tokenize.ENDMARKER:
-        print "Habemus ENDMARKER "
+        # print "Habemus ENDMARKER "
         raise EOFError
     else:
         print "Habemus splat"
         raise SyntaxError
 
-t = '+1 2 +1 3 2'
+#t = '+1 2 +1 3 2'
 #t = '+1'
 #t = '+1 2'
+t = '+1 2 -1 3 2'
 
 tokenizer = tokenize.generate_tokens(iter([t]).next)
 
 print reduce(tokenizer)
 
 tokenizer.close()
-print 'Bye-bye World'
