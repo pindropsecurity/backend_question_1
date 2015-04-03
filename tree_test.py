71ec78d48618889be46b0cc82563eff42c3d2df3
renacuajo67 <rene@ieee.org>
2015-04-03 19:48:06 -0400

    Basic tests

--- tree_test.py
+++ tree_test.py
@@ -2,8 +2,23 @@ import unittest
 import tree
 
 class TestTreeFunction(unittest.TestCase):
-    def test_hello(self):
-        kk = tree.tree('+ 1 2')
+    def test_basicPlus(self):
         self.assertEqual(tree.tree('+ 1 2'), 3, 'incorrect addition of + 1 2')
+
+    def test_basicMinus(self):
+        self.assertEqual(tree.tree('- 1 2'), -1, 'incorrect addition of + 1 2')
+
+    def test_basicMult(self):
+        self.assertEqual(tree.tree('* 4 2'), 8, 'incorrect addition of + 1 2')
+
+    def test_basicDiv(self):
+        self.assertEqual(tree.tree('/ 4 2'), 2, 'incorrect addition of + 1 2')
+
+    def test_badOperator(self):
+        with self.assertRaises(AssertionError):
+            tree.operate('%', 4, 2)
+
+    def test_moreThan2(self):
+        self.assertEqual(tree.tree('+ 1 2 3 4'), 10, 'incorrect addition of + 1 2')
 
 unittest.main()
