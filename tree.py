046ec39fabebd86c90aa1958c2a0a0f29b396ef2
renacuajo67 <rene@ieee.org>
2015-04-03 12:15:48 -0400

    Got the token library to work

--- tree.py
+++ tree.py
@@ -1,6 +1,12 @@
+import tokenize, token
+
 print 'Hello World'
 
 class tree:
-    reduce
+    def __init__(self, inStr):
+        for t in tokenize.generate_tokens(iter([inStr]).next):
+            print token.tok_name[t[0]],
+        print
 
+t = tree('[+ [1 2]]')
 print 'Bye-bye World'
