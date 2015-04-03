97773226826b92ce76331119ad9bb73dbb3c75c8
renacuajo67 <rene@ieee.org>
2015-04-03 17:05:35 -0400

    Check point my work

--- tree.py
+++ tree.py
@@ -1,21 +1,27 @@
 import tokenize 
 
 def reduce(tokenizer):
+
     toktyp,tokval, _,_,_ = tokenizer.next()
 
     if toktyp == tokenize.OP and '+' in tokval:
-       return reduce(tokenizer)
+        accum = 0
+        while True:
+            try:
+                accum += reduce(tokenizer)
+            except StopIteration:
+                return accum
 
     elif toktyp == tokenize.NUMBER:
-       print "Habemus NUMBER " + tokval
-       return int(tokval)
+        print "Habemus NUMBER " + tokval
+        return int(tokval)
     elif toktyp == tokenize.ENDMARKER:
-       print "Habemus ENDMARKER "
-       return
+        print "Habemus ENDMARKER "
+        return 0
     else:
-       print "Habemus splat"
+        print "Habemus splat"
 
-t = '+1 2 3'
+t = '+1 2 +1 3 + +2'
 
 tokenizer = tokenize.generate_tokens(iter([t]).next)
 
