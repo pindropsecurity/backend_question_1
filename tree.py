c3dc3b87b55cec07689517e6e92efbee9e5265b5
renacuajo67 <rene@ieee.org>
2015-04-03 15:59:03 -0400

    Check point my work

--- tree.py
+++ tree.py
@@ -3,29 +3,29 @@ import tokenize
 class tree:
     def __init__(self, tokenizer):
         toktyp,tokval, _,_,_ = tokenizer.next()
-        print toktyp
-        print tokval
         if toktyp == tokenize.OP and '+' in tokval:
             print "Habemus +"
             tree(tokenizer)
         elif toktyp == tokenize.OP and '-' in tokval:
             print "Habemus -"
+            tree(tokenizer)
         elif toktyp == tokenize.OP and '*' in tokval:
             print "Habemus *"
+            tree(tokenizer)
         elif toktyp == tokenize.OP and '/' in tokval:
             print "Habemus /"
+            tree(tokenizer)
+        elif toktyp == tokenize.NUMBER:
+            print "Habemus NUMBER " + tokval
+            return tokval;
         else:
             print "Habemus splat"
 
 t = '+1 2 - 3 2'
 
 tokenizer = tokenize.generate_tokens(iter([t]).next)
-toktyp,tokval, _,_,_ = tokenizer.next()
 
-if toktyp == tokenize.OP or '[' in tokval:
-   tree(tokenizer)
-else:
-   print 'bad tree'
+tree(tokenizer)
 
 tokenizer.close()
 print 'Bye-bye World'
