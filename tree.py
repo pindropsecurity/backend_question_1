7fcc6db9ba37f7f44cb6247f6ed4360f34ba4a32
renacuajo67 <rene@ieee.org>
2015-04-03 12:42:00 -0400

    Identify the tokens we care to keyoff

--- tree.py
+++ tree.py
@@ -4,8 +4,17 @@ print 'Hello World'
 
 class tree:
     def __init__(self, inStr):
-        for t in tokenize.generate_tokens(iter([inStr]).next):
-            print token.tok_name[t[0]],
+        for toktyp, toksym, _, _, _ in tokenize.generate_tokens(iter([inStr]).next):
+            if toktyp == tokenize.NUMBER:
+                print 'THIS IS A NUMBER'
+            elif toktyp == tokenize.OP and '[' in toksym:
+                print 'THIS IS A ['
+            elif toktyp == tokenize.OP and ']' in toksym:
+                print 'THIS IS A ]'
+            elif toktyp == tokenize.ENDMARKER:
+                print 'THE END'
+            else:
+                print 'THIS IS A SPLAT'
         print
 
 t = tree('[+ [1 2]]')
