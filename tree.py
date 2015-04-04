16a6999257fbe641f5c28c4488e9ebf97254e029
renacuajo67 <rene@ieee.org>
2015-04-04 09:42:58 -0400

    Add some comments

--- tree.py
+++ tree.py
@@ -1,6 +1,39 @@
 import tokenize 
 
 #
+# DESIGN NOTES:
+#
+# 1)-I chose not to use classes because there is little state to be stored in a would
+#    be object. Having it as a library suffices. Of course the depth could be seen
+#    as a state for an object, but the stack does an good job of storing it.
+#
+# 2)-I assumed a string representation for the trees. The only valid tokens are
+#    NUMBERS and OPERATORS as specified in the program. However these language
+#    elements are insufficient to express trees that are taller on the left branch.
+#    For example:
+#         +
+#       /   \
+#     +      1
+#   /  \
+# 1     2
+#
+#    could not be expressed in my grammar. A dfs traversal of the labels would give us
+#    + + 1 2 1
+#    This would be interpreted as:
+#         +
+#       /
+#     + 
+#   /  \  \
+# 1     2   1
+#    This does not belong to the grammar given that the root is node of arity 1.
+#    To address this we would have had to add parenthesis or some other symbol
+#    for associating.
+#
+# 3)-I do not have a 'debug mode' in the code. To achieve this we need to uncomment
+#    the print statements in the code.
+#
+
+#
 # Set of valid operators
 #
 operators = ['+', '-', '*', '/']
@@ -21,6 +54,12 @@ def operate(op, val1, val2):
        assert False, 'Lost all hope'
 
 
+#
+# reduce - Given a generator that represents a tree reduce via the
+#          mathematical operations
+#
+#   Chose tu use the tokenizer library to avoid writing a lexer.
+#
 def reduce(tokenizer, depth):
 
     childcount = 0
@@ -82,4 +121,3 @@ def tree (t):
     tokenizer.close()
     return result
 
-#tree('+ + + 1 2 3')
