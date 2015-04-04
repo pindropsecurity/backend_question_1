a1699786a161e40b1624516e9c366f2f32bd543a
renacuajo67 <rene@ieee.org>
2015-04-03 21:00:40 -0400

    Get + + 1 2 + 3 4 to test

--- tree_test.py
+++ tree_test.py
@@ -11,8 +11,8 @@ class TestTreeFunction(unittest.TestCase):
     def test_basicMult(self):
         self.assertEqual(tree.tree('* 4 2'), 8, 'incorrect multiplication of * 4 2')
 
-    #def test_basicDiv(self):
-        #self.assertEqual(tree.tree('/ 4 -2'), -2, 'incorrect division of / 4 -2')
+    def test_basicDiv(self):
+        self.assertEqual(tree.tree('/ 4 2'), 2, 'incorrect division of / 4 -2')
 
     def test_badOperator(self):
         with self.assertRaises(AssertionError):
@@ -27,10 +27,14 @@ class TestTreeFunction(unittest.TestCase):
     def test_MultsmoreThan2(self):
         self.assertEqual(tree.tree('* 1 2 3 4'), 24, 'incorrect addition of + 1 2 3 4')
 
-    #def test_DivmoreThan2(self):
-        #self.assertEqual(tree.tree('/ 1000 -10 -10'), 10, 'incorrect addition of + 1000 -10 -10')
+    def test_DivmoreThan2(self):
+        self.assertEqual(tree.tree('/ 1000 10 10'), 10, 'incorrect addition of + 1000 10 10')
 
     def test_PlusHeight2(self):
-        self.assertEqual(tree.tree('+ 2 4 + 1 2'), 9, 'incorrect addition of + 1000 -10 -10')
+        self.assertEqual(tree.tree('+ 2 4 + 1 2'), 9, 'incorrect addition of + 2 4 + 1 2')
+
+    def test_PlusHeight2Bal(self):
+        self.assertEqual(tree.tree('+ + 1 2 + 3 4'), 10, 'incorrect addition of + + 1 2 + 3 4')
+
 
 unittest.main()
