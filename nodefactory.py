0bd339ef78a8a41d1e3c8ba5c5b829c17e64711b
Abel Mohler <abel@wayfarerweb.com>
2015-04-06 02:34:02 -0400

    Removal of unneeded Tree class, extra features on Node class. Reorganization of math_tree_calculator to be more encapsulated. Features added to make testing easier, including random tree generation. Lots of tests.

--- nodefactory.py
+++ nodefactory.py
@@ -1,10 +1,3 @@
-class ParentDoesNotExist(Exception):
-    pass
-
-
-class SiblingDoesNotExist(Exception):
-    pass
-
 
 class InvalidMathNode(Exception):
     pass
@@ -12,72 +5,11 @@ class InvalidMathNode(Exception):
 
 class Node(list):
     """
-    A Node is a nothing more than a list that tracks a little extra information about where it exists
-    in relation to other nodes
+    A Node is a nothing more than a list that tracks values
     """
-    position = 0
-    depth = 0
-    def __init__(self, value, children=[], parent=None, *args, **kwargs):
+    def __init__(self, value, *args, **kwargs):
         self.value = value
-        for child in self:
-            self.add_child(child)
-        self.parent = parent
         super(Node, self).__init__(*args, **kwargs)
-    
-    def add_child(self, node):
-        node.parent = self
-        node.position = len(self)
-        node.depth = self.depth + 1
-        self.append(node)
-
-
-class Tree(object):
-    """
-    Acts as a 'container' for Node objects, to be organized in a tree structure with a single base node.
-    """
-    def __init__(self, root):
-        if not isinstance(root, Node):
-            raise TypeError('root must be of a Node or subclass of Node')
-        self.root = root
-        self.reset()
-    
-    def reset(self):
-        self.current = self.root
-    
-    def move_to(self, node):
-        """Wrapper for moving current pointer"""
-        self.current = node
-    
-    def move_up(self):
-        """Move to the parent node if it exists"""
-        if not isinstance(self.current.parent, Node):
-            raise ParentDoesNotExist('Parent node does not exist')
-        self.move_to(self.current.parent)
-    
-    def next(self):
-        """Move to the next sibling of the current node, if it exists"""
-        proposed_position = self.current.position + 1
-        try:
-            proposed_current = self.current.parent[proposed_position]
-        except IndexError:
-            #: if next sibling doesn't exist
-            self.handle_sibling_error(self.current.depth, proposed_position)
-        except TypeError:
-            #: if parent is None
-            self.handle_sibling_error(self.current.depth, proposed_position)
-        else:
-            self.move_to(proposed_current)
-        return self.current
-    
-    def previous(self):
-        """Move to the previous sibling of the current node, if it exists"""
-        if self.current.position == 0:
-            self.handle_sibling_error(self.current.depth, -1)
-        self.move_to(self.current.parent[self.current.position - 1])
-        return self.current
-    
-    def handle_sibling_error(self, depth, position):
-        raise SiblingDoesNotExist('Node does not exist at depth: %d, position: %d' % (depth, position))
 
 
 class MathNode(Node):
@@ -103,25 +35,26 @@ class MathNode(Node):
         super(MathNode, self).__init__(value, *args, **kwargs)
 
 
-def _get_total(math_mode, left, right):
-    if left == None:
-        return right
-    total = left
-    if math_mode == '-':
-        total -= right
-    elif math_mode == '+':
-        total += right
-    elif math_mode == '/':
-        total /= right
-    elif math_mode == '*':
-        total *= right
-    return total
-
-
 def math_tree_calculator(node, total=None, math_mode=None):
+    """Recursively loop over an entire 'math node tree', calculate its total value"""
+    def _get_total(math_mode, left, right):
+        if left == None:
+            return right#: Happens if there's no total yet
+        elif right == None:
+            return left#: Can happen with malformed tree
+        total = left
+        if math_mode == '-':
+            total -= right
+        elif math_mode == '+':
+            total += right
+        elif math_mode == '/':
+            total /= float(right)
+        elif math_mode == '*':
+            total *= right
+        return total
+    
     if node.nodetype == 'operator':
         for child in node:
-            print child.nodetype
             if child.nodetype == 'number':
                 total = _get_total(node.value, total, child.value)
             else:
@@ -132,22 +65,62 @@ def math_tree_calculator(node, total=None, math_mode=None):
 
 
 def node_pretty_print(node, indent=0):
+    """Prints out nodes for nice display during testing"""
+    optional_twig = ''
+    optional_branch = ''
     rjust_indent = indent
     if indent:
-        rjust_indent = indent * 3
-    print '{}'.format(node.value).rjust(rjust_indent, ' ')
+        rjust_indent = indent * 4
+        optional_twig = ' {'
+    if len(node):
+        optional_branch = '\\'
+    print '{}{}'.format(optional_twig, node.value).rjust(rjust_indent, ' ')
+    if optional_branch:
+        print optional_branch.rjust(rjust_indent + 2)
     for child in node:
         node_pretty_print(child, indent + 1)
 
 
 def dict_to_math_node(d):
+    """Converts dict representations of nodes into actual Node objects"""
     node = MathNode(d['value'])
     if 'children' in d:
         for child in d['children']:
             child_node = dict_to_math_node(child)
-            node.add_child(child_node)
+            node.append(child_node)
     return node
 
 
 def list_to_math_tree(l):
+    """Wrapper for dict_to_math_node, allows creation of lists that more resemble our actual node trees"""
     return dict_to_math_node(l[0])
+
+
+def create_random_mathnodetree(depth=0):
+    """Creates random node and calculates, for fun, and testing unexpected situations"""
+    import random
+    do_operator = False
+    if depth == 0:
+        do_operator = True
+    else:
+        base_chance = 60
+        adjusted_chance = base_chance - (depth * 2)
+        randint = random.randint(0, 100)
+        if randint <= adjusted_chance:
+            do_operator = True
+    
+    if do_operator:
+        value = random.choice(['-', '+', '/', '*'])
+    else:
+        value = random.randint(1, 40)
+    node = MathNode(value=value)
+    
+    if do_operator:
+        if depth == 0:
+            #: Make sure we at least go one deep
+            child_length = random.randint(1, 6)
+        else:
+            child_length = random.randint(0, 5)
+        for i in range(child_length):
+            node.append(create_random_mathnodetree(depth=depth+1))
+    return node
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-from nodefactory import list_to_math_tree, math_tree_calculator, node_pretty_print
+from nodefactory import list_to_math_tree, math_tree_calculator, node_pretty_print, create_random_mathnodetree
+
 
 def unittest(l, expected_result):
     node_tree = list_to_math_tree(l)
@@ -10,14 +11,15 @@ def unittest(l, expected_result):
     print 'Total: {}'.format(total)
     print 'Expected total: {}'.format(expected_result)
     assert total == expected_result
+    print '---------'
+
 
 test_1_list = [
     {
         'value': '*',
         'children': [
             {
-                'value': 3,
-                'children': []
+                'value': 3
             },
             {
                 'value': 2
@@ -38,8 +40,208 @@ test_1_list = [
 ]
 
 def test_1():
+    """Normal, 2 levels deep"""
     unittest(test_1_list, 42)
+
+
+test_2_list = [
+    {
+        'value': '-',
+        'children': []
+    }
+]
+
+
+def test_2():
+    """Missing children"""
+    unittest(test_2_list, None)
+
+
+test_3_list = [
+    {
+        'value': '+',
+        'children': [
+            {
+                'value': '*'
+            }
+        ]
+    }
+]
+
+
+def test_3():
+    """Non-numeric value in leaf node"""
+    unittest(test_3_list, None)
+
+
+test_4_list = [
+    {
+        'value': '*',
+        'children': [
+            {
+                'value': 3,
+            },
+            {
+                'value': 5,
+                'children': [
+                    {
+                        'value': 6
+                    },
+                    {
+                        'value': 9
+                    },
+                    {
+                        'value': '/',
+                        'children': [
+                            {
+                                'value': 8
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                'value': '+',
+                'children': [
+                    {
+                        'value': 4
+                    },
+                    {
+                        'value': 2
+                    }
+                ]
+            },
+            {
+                'value': 2,
+            },
+        ]
+    }
+]
+
+
+def test_4():
+    """Children in numeric nodes"""
+    unittest(test_4_list, 180)
+
+
+test_5_list = [
+    {
+        'value': '+',
+        'children': [
+            {
+                'value': 2.0
+            },
+            {
+                'value': '*',
+                'children': [
+                    {
+                        'value': 2
+                    },
+                    {
+                        'value': '+',
+                        'children': [
+                            {
+                                'value': 1
+                            },
+                            {
+                                'value': 2
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                'value': '/',
+                'children': [
+                    {
+                        'value': 5.0
+                    },
+                    {
+                        'value': 2
+                    }
+                ]
+            },
+            {
+                'value': '-',
+                'children': [
+                    {
+                        'value': 10
+                    },
+                    {
+                        'value': 5
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+
+def test_5():
+    """Every operator"""
+    unittest(test_5_list, 15.5)
+
+
+test_6_list = [
+    {
+        'value': '+',
+        'children': [
+            {
+                'value': '+',
+                'children': [
+                    {
+                        'value': '+',
+                        'children': [
+                            {
+                                'value': '+',
+                                'children': [
+                                    {
+                                        'value': 10
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                'value': '+',
+                'children': [
+                    {
+                        'value': '+',
+                        'children': [
+                            {
+                                'value': 4
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+
+def test_6():
+    """Lots of levels"""
+    unittest(test_6_list, 14)
+
+
+def do_optional_test():
+    do_test = raw_input('Generate random node and calculate? (y/n) ')
+    if do_test == 'y':
+        random_node = create_random_mathnodetree()
+        node_pretty_print(random_node)
+        total = math_tree_calculator(random_node)
+        print 'Total: {}'.format(total)
 
 
 if __name__ == '__main__':
     test_1()
+    test_2()
+    test_3()
+    test_4()
+    test_5()
+    test_6()
+    do_optional_test()
+    
