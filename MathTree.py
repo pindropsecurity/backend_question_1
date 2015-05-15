3d593b498cf02af35e65eb18a69d6de56e426e93
James Cooper <EEdeUSF@gmail.com>
2015-05-15 16:12:31 +0000

    -Rearranged Node Classes to have seperate Branch and Leaf classes -Created new Node Class for dynamically generating nodes

--- MathTree.py
+++ MathTree.py
@@ -1,22 +1,33 @@
 import re
 
+numberForm = re.compile("^-?\d*\.?\d*$")
+operationForm = re.compile("^[+-/*/]$")
+
 class Node(object):
-  numberForm = re.compile("^-?\d*\.?\d*$")
-  operationForm = re.compile("^[+-/*/]$")
+  def __new__(cls, value, *children):
+    strValue = str(value)
+    if operationForm.match(strValue):
+      obj = super().__new__(BranchNode)
+      obj.__init__(value, *children)
+      return obj
+    elif numberForm.match(strValue):
+      obj = super().__new__(LeafNode)
+      obj.__init__(value, *children)
+      return obj
+    else:
+      raise ValueError("""'value' must be a real number, or a string
+                        representing a real number or operator (+-/*)""")
+
+class BranchNode(object):
   children = []
 
   def __init__(self, value, *children, isRoot=False):
-    if self.numberForm.match(value):
-      self.value = float(value)
-      self.isLeaf = True
-      if isRoot:
-        raise ValueError("Root node must have value '+', '-', '*', or '/'")
-    elif self.operationForm.match(value):
+    if operationForm.match(value):
       self.value = value
-      self.isLeaf = False
-      self.isRoot = isRoot
     else:
-      raise ValueError("'Value' must be a number string or an operator (+-*/)")
+      raise ValueError("""'value' must an operator (+-*/) for instantiating a
+                        BranchNode. For mixed Node instantiation, please use
+                        Node(value, *children).""")
     for arg in children:
       if isinstance(arg, list) | isinstance(arg, tuple):
         for child in arg:
@@ -29,12 +40,15 @@ class Node(object):
       raise TypeError("Child node must be of type Node")
     else:
       self.children.append(child)
-      self.isLeaf = False
 
-  def __len__(self):
-    total = 0
-    if self.isLeaf | (len(self.children) < 1):
-      return 1
-    for child in self.children:
-      total += len(child)
-    return total
+class LeafNode(Node):
+  def __init__(self, value, *children):
+    strValue = str(value)
+    if numberForm.match(strValue):
+      self.value = float(value)
+    else:
+      raise ValueError("""'value' must be a real number for instantiating a
+                        LeafNode. For mixed Node instantiation, please use
+                        Node(value, *children).""")
+    if len(children) > 0:
+      raise TypeError("""A LeafNode cannot have child nodes.""")
--- MathTree_test.py
+++ MathTree_test.py
@@ -1,20 +1,26 @@
 import unittest
-from MathTree import Node
+from MathTree import Node, BranchNode, LeafNode
 
 class NodeTestCase(unittest.TestCase):
   def setUp(self):
-    self.child_node_a = Node('3')
-    self.child_node_b = Node('-12.18')
-    self.parent_node = Node('+', 
-                            (self.child_node_a, self.child_node_b),
-                            Node('-',Node('4'),Node('2')),
-                            isRoot=True)
+    self.child_node_A = Node('-3.14')
+    self.child_node_B = Node(2)
+    self.parent_node = Node('+')
 
-  def test_addChild(self):
-    self.assertEqual(len(self.parent_node),5)
+  def test_BranchNode_Instantiation(self):
+    self.assertTrue(isinstance(self.parent_node, BranchNode))
 
-  def test_str_cast(self):
-    self.assertEqual(str(self.parent_node),"(3)+(-12.18)+(4-2)")
+  def test_LeafNode_Instantiate(self):
+    self.assertTrue(isinstance(self.child_node_A, LeafNode))
+    self.assertTrue(isinstance(self.child_node_B, LeafNode))
+    self.assertFalse(isinstance(self.child_node_A, BranchNode))
+    self.assertFalse(isinstance(self.child_node_B, BranchNode))
+
+  # def test_addChild(self):
+  #   self.assertEqual(len(self.parent_node),5)
+
+  # def test_str_cast(self):
+  #   self.assertEqual(str(self.parent_node),"((3)+(-12.18)+((4)-(2)))")
 
 if __name__ == "__main__":
   unittest.main()
